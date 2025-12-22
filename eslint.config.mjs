ceb1d13ea8ea826553b0e33bf83f30ff446cef0b
hyunjun9788 <namhyunjun9788@gmail.com>
2025-12-23 00:16:11 +0900

    feat: 초기 세팅 (prettier,eslint,tailwind, shadcn,fsd 패턴)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,52 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import prettierPlugin from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
-export default eslintConfig;
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+})
+
+export default [
+  // 기본 Next.js + TypeScript 설정
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+
+  // Prettier 연동
+  {
+    files: ['**/*.{js,ts,jsx,tsx}'],
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      'require-jsdoc': 'off',
+      'react/display-name': 'off',
+      'react-hooks/exhaustive-deps': 'warn',
+      'prettier/prettier': 'error',
+      'jsx-a11y/anchor-is-valid': [
+        'error',
+        {
+          components: ['Link'],
+          specialLink: ['hrefLeft', 'hrefRight'],
+          aspects: ['invalidHref', 'preferButton'],
+        },
+      ],
+    },
+  },
+
+  // Prettier 룰 무력화 (중복 방지)
+  prettierConfig,
+]
