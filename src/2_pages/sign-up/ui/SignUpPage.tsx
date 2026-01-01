2a958e4cf6fa6ec64f67258b555365b371415243
hyunjun9788 <namhyunjun9788@gmail.com>
2026-01-02 00:33:40 +0900

    feat: #1 입력 필드 추가

--- src/2_pages/sign-up/ui/SignUpPage.tsx
+++ src/2_pages/sign-up/ui/SignUpPage.tsx
@@ -8,8 +8,9 @@ export default function SignUpPage() {
 
       <div className="flex flex-col items-center">
         <h1 className="mb-2 text-3xl font-bold">회원가입</h1>
-        <p className="text-gray-500">
-          대구 한국가스공사 팬 커뮤니티에 오신 것을 환영합니다
+        <p className="flex flex-col items-center text-gray-500">
+          <span>대구 한국가스공사 팬 커뮤니티에 오신 것을</span>
+          <span>환영합니다</span>
         </p>
       </div>
       <SignUpSection />
--- src/4_features/sign-up/ui/SignUpForm.tsx
+++ src/4_features/sign-up/ui/SignUpForm.tsx
@@ -21,12 +21,33 @@ export const SignUpForm = () => {
       nickname: '',
     },
   })
+
   const onSubmit = (data: SignUpFormValues) => {
     console.log('회원가입 요청 데이터:', data)
   }
+
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="mt-20 flex w-full flex-col gap-5"
+      >
+        <FormField
+          control={form.control}
+          name="nickname"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>닉네임</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="ex) 엔젤란겔"
+                  className="text-4 h-12.5 md:text-sm"
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
         <FormField
           control={form.control}
           name="email"
@@ -36,7 +57,39 @@ export const SignUpForm = () => {
               <FormControl>
                 <Input
                   placeholder="user@example.com"
-                  className="text-[16px] md:text-sm"
+                  className="text-4 h-12.5 md:text-sm"
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>비밀번호</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="8자 이상 입력"
+                  className="text-4 h-12.5 md:text-sm"
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="confirmPassword"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>비밀번호 확인</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="비밀번호 재입력"
+                  className="text-4 h-12.5 md:text-sm"
                   {...field}
                 />
               </FormControl>
