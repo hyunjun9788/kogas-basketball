75dbfd320e33edd92df7f71598fc86a8559b6ea7
hyunjun9788 <namhyunjun9788@gmail.com>
2026-01-03 19:17:17 +0900

    style: #1 인풋 스타일 수정

--- src/6_shared/ui/Form.tsx
+++ src/6_shared/ui/Form.tsx
@@ -98,7 +98,7 @@ function FormLabel({
     <Label
       data-slot="form-label"
       data-error={!!error}
-      className={cn('data-[error=true]:text-destructive', className)}
+      className={className}
       htmlFor={formItemId}
       {...props}
     />
@@ -148,7 +148,7 @@ function FormMessage({ className, ...props }: React.ComponentProps<'p'>) {
     <p
       data-slot="form-message"
       id={formMessageId}
-      className={cn('text-destructive text-sm', className)}
+      className={cn('text-main text-sm', className)}
       {...props}
     >
       {body}
