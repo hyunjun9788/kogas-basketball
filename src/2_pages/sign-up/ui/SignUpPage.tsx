b75105856f995edd1242b85cbda1080c33d4c162
hyunjun9788 <namhyunjun9788@gmail.com>
2026-01-03 19:20:42 +0900

    style: #1 회원가입 텍스트 색상 수정

--- src/2_pages/sign-up/ui/SignUpPage.tsx
+++ src/2_pages/sign-up/ui/SignUpPage.tsx
@@ -7,7 +7,7 @@ export default function SignUpPage() {
       <div className="flex flex-col items-center">
         <KogasIcon className="h-50 w-50" />
 
-        <h1 className="mb-2 text-3xl font-bold">회원가입</h1>
+        <h1 className="text-main mb-2 text-3xl font-bold">회원가입</h1>
         <p className="flex flex-col items-center text-gray-500">
           <span>대구 한국가스공사 팬 커뮤니티에 오신 것을</span>
           <span>환영합니다</span>
